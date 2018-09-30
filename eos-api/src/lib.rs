eb5b46866d9085af5ce09d28d6f8cde3c14d2f80
datudou <wangqianyi.cs@gmail.com>
2018-10-01 01:35:58 +0800

    use borrow instead  move

--- eos-api/src/lib.rs
+++ eos-api/src/lib.rs
@@ -5,50 +5,49 @@ extern crate serde_json;
 use reqwest::Error;
 use serde_json::Value;
 
-
-#[derive(Copy, Clone)]
+#[derive(Clone, Debug)]
 pub struct API {
-    base_url: &'static str,
+    base_url: String,
 }
 
 
 impl API {
-    pub fn new(base_url: &'static str) -> API {
+    pub fn new(base_url: String) -> API {
         API { base_url }
     }
 
     // net api
-    pub fn get_connections(self) -> Result<Value, Error> {
+    pub fn get_connections(&self) -> Result<Value, Error> {
         self.http_get("/net/connections")
     }
 
     //chain api
-    pub fn get_chain_info(self) -> Result<Value, Error> {
+    pub fn get_chain_info(&self) -> Result<Value, Error> {
         self.http_get("/chain/get_info")
     }
 
-    pub fn get_account(self, account_name: &str) -> Result<Value, Error> {
+    pub fn get_account(&self, account_name: &str) -> Result<Value, Error> {
         let body = &json!({ "account_name": account_name });
         self.http_post("/chain/get_account", body)
     }
 
-    pub fn get_abi(self, account_name: &str) -> Result<Value, Error> {
+    pub fn get_abi(&self, account_name: &str) -> Result<Value, Error> {
         let body = &json!({ "account_name": account_name });
         self.http_post("/chain/get_abi", body)
     }
 
-    pub fn get_block(self, block_id: u64) -> Result<Value, Error> {
+    pub fn get_block(&self, block_id: u64) -> Result<Value, Error> {
         let body = &json!({ "block_num_or_id": block_id });
         self.http_post("/chain/get_block", body)
     }
 
-    pub fn get_code(self, account: &str) -> Result<Value, Error> {
+    pub fn get_code(&self, account: &str) -> Result<Value, Error> {
         let body = &json!({"account_name": account, "code_as_wasm": false});
         self.http_post("/chain/get_code", body)
     }
 
     pub fn get_currency_balance(
-        self,
+        &self,
         account_name: &str,
         code: &str,
         symbol: &str,
@@ -73,10 +72,8 @@ impl API {
         let res = reqwest::Client::new()
             .post(&url)
             .json(body)
-            .send()
-            .unwrap()
-            .json()
-            .unwrap();
+            .send()?
+            .json()?;
         Ok(res)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,20 @@ extern crate eos_api;
 use eos_api::API;
 
 fn main() {
-    let eos_cli = API::new("https://api.eoslaomao.com");
+    let eos_cli = API::new("https://api.eoslaomao.com".to_string());
 
     match eos_cli.get_currency_balance("eoslaomaocom", "eosio.token", "EOS") {
-        Ok(info) => print!("{:#?}", info),
-        Err(_) => println!("1111"),
+        Ok(res) => println!("{:#?}", res),
+        Err(error) => println!("{:#?}", error),
+    };
+
+    match eos_cli.get_chain_info() {
+        Ok(res) => println!("{:#?}", res),
+        Err(error) => println!("{:#?}", error),
+    }
+
+    match eos_cli.get_connections() {
+        Ok(res) => println!("{:#?}", res),
+        Err(error) => println!("{:#?}", error),
     }
 }
